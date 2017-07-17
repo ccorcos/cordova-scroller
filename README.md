8808208ab13d29d0c397c3249e0db08f55009690
Chet Corcos <ccorcos@gmail.com>
2017-07-17 16:00:22 -0700

    first

---
+++ README.md
@@ -0,0 +1,16 @@
+# Testing Out iFrames in Cordova
+
+
+Run a dev server:
+```
+npm run server
+````
+
+Set everything up:
+```
+npm install
+npm run setup
+npm run xcode
+```
+
+Build to a simulator.
---
+++ www/js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("deviceready", function() {
+	document.body.style.margin = 0
+	document.body.style.padding = 0
+	document.body.style.overflow = "hidden"
+
+	var input = document.createElement("input")
+	input.style.marginTop = 50
+	input.style.marginLeft = "auto"
+	input.style.marginRight = "auto"
+	document.body.appendChild(input)
+})
